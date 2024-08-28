7ee62b0ff171ae5aa656e9161d4bd6f5b28e6774
Prashant Nepal <prashantnepal074@gmail.com>
2024-08-28 16:17:40 +0545

    login API

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const { registerController } = require('../controllers/authControllers,js')
+const { logincontroller } = require('../controllers/authControllers,js')
 
 const router = express.Router()
 
 //route
 //register || POST
 router.post('/register', registerController)
+
+//login || POST
+router.post('/login', logincontroller)
 module.exports = router 
